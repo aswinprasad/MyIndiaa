cf026a9732a14ddd68d532233ec87d3b48bf4923
aswin.p <aswin.p@twinleaves.co>
2024-06-23 17:59:01 +0530

    fixed pwa install

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,22 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 const manifestForPlugIn = {
   registerType: 'autoUpdate',
-  includeAssests: ['favicon.ico', 'apple-touch-icon.png'],
   manifest: {
     name: 'MyIndiaa',
     short_name: 'MyIndiaa',
     description: 'Empowering India with dropshipping business',
     icons: [
       {
-        src: '/android-chrome-192x192.png',
+        src: '/pwa-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'favicon',
+        purpose: 'any',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: '/pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'favicon',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
+        purpose: 'any',
       },
       {
         src: '/pwa-maskable-192x192.png',
