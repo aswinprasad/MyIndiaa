f45776f61a2a2dbf63eef709e06f08eccf7963f2
aswin.p <aswin.p@twinleaves.co>
2024-06-24 19:48:20 +0530

    Update useNetworkStatus.js

--- src/hooks/useNetworkStatus.js
+++ src/hooks/useNetworkStatus.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 
-const useNetworkStatus = () => {
+const useNetworkStatus = (
+  interval = 5000,
+  pingUrl = 'https://www.google.com'
+) => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
   useEffect(() => {
@@ -9,11 +12,18 @@ const useNetworkStatus = () => {
     window.addEventListener('online', updateOnlineStatus);
     window.addEventListener('offline', updateOnlineStatus);
 
+    const intervalId = setInterval(() => {
+      fetch(pingUrl, { method: 'HEAD', mode: 'no-cors' })
+        .then(() => setIsOnline(true))
+        .catch(() => setIsOnline(false));
+    }, interval);
+
     return () => {
       window.removeEventListener('online', updateOnlineStatus);
       window.removeEventListener('offline', updateOnlineStatus);
+      clearInterval(intervalId);
     };
-  }, []);
+  }, [interval, pingUrl]);
 
   return isOnline;
 };
